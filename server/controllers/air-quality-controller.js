26c68dcaa12fd8fa16a1fc389531b6e7111bf073
Connor Rose <connorrose.git@gmail.com>
2020-09-09 08:58:57 -0700

    create fire and aqi controllers

---
+++ server/controllers/air-quality-controller.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const { AirQuality } = require('../models/air-quality-model');
+
+const { BREEZOMETER_API } = process.env;
+
+async function getAirQuality({ latitude, longitude }) {
+  try {
+    let aqi;
+    const [result] = await AirQuality.find({ latitude, longitude });
+
+    if (result !== undefined) {
+      aqi = result.aqi;
+    } else {
+      const { data } = await axios.get(
+        `https://api.breezometer.com/air-quality/v2/current-conditions?lat=${latitude}&lon=${longitude}&key=${BREEZOMETER_API}`
+      );
+      aqi = data.data.indexes.aqi;
+      AirQuality.create({ latitude, longitude, aqi }).catch((err) =>
+        console.error(`ERROR writing AirQuality to DB: ${err}`)
+      );
+    }
+
+    return aqi;
+  } catch (err) {
+    return NaN;
+  }
+}
+
+module.exports = {
+  getAirQuality,
+};
